1491eae8e7d4092ce48a8be3b191439052ff5579
Nagy János <nagy@zold.lan>
2024-03-05 18:32:23 +0100

    First

--- script/script.js
+++ script/script.js
@@ -3,5 +3,27 @@ console.log("Helló")
 cella = document.createElement('div')
 // cella.className="cella"
 cella.classList.add("cella")
+// cella.setAttribute('cicamica',' Igor')
+cella.dataset.kep="./img/1.png"
+//cella.style.backgroundImage='url("./img/2.png")'
+// cella.onclick = 
+function balKattintas(){
+    console.log(this.style.backgroundImage)
+    console.log(this.dataset.kep)
+    // this.style.backgroundImage='url("'+this.dataset.kep+'")'
+    this.style.backgroundImage=`url("${this.dataset.kep}")`
+    setTimeout(() => {
+        this.style.backgroundImage="none"
+    }, 3000);    
+} 
+
+katt = () => {
+    console.log(this)
+}
+
+cella.addEventListener("click", balKattintas )
+
+
+
 
 document.getElementById("palya").appendChild(cella)
